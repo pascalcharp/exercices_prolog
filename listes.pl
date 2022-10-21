29c7f98f60c4fe5630f8770a0e7ff5640f6a5235
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-10-21 13:18:12 -0400

    Plein d'ajouts, en particulier le tri fusion

--- listes.pl
+++ listes.pl
@@ -20,6 +20,20 @@ element(E, [E|_]) :- !.
 element(E, [_|Xs]) :- element(E, Xs).
 
 %
+% element(E, L, N) est vrai si N est le nombre d'occurrence de E dans L.
+%
+
+element(_, [], 0).
+
+element(E, [E|Xs], N) :-
+    element(E, Xs, N1),
+    N is N1 + 1.
+
+element(E, [X|Xs], N) :-  
+    \+ (X=E),
+    element(E, Xs, N).
+
+%
 % concatenation(L1, L2, L) est vrai si L est la concaténation des listes L1|L2
 %   
 
@@ -32,6 +46,13 @@ concatenation([X|Xs], L1, [X|Ls]) :- concatenation(Xs, L1, Ls).
 %
 
 prefixe(P, L) :- concatenation(P, _, L).
+
+%
+% prefixe(P, L, M) est vrai si P est un préfixe de la liste L, et que M est 
+% identique à L sans le préfixe.
+%
+
+prefixe(P, L, M) :- concatenation(P, M, L).
 
 %
 % suffixe(S, L) est vrai si S est un suffixe de la liste L, y-compris L elle-même
@@ -48,6 +69,33 @@ sous_liste(S, L) :- prefixe(S, L), !.
 sous_liste(S, [_|Xs]) :- sous_liste(S, Xs).
 
 %
+% sous_liste(S, L, N) est vrai si S est une sous-liste de L débutant 
+% à la position N
+%
+
+sous_liste(S, L, 0) :- prefixe(S, L), !.
+
+sous_liste(S, [_|Xs], N) :- sous_liste(S, Xs, N1), N is N1 + 1.
+
+%
+% sous_liste(S, L, N, M) est vrai si S est une sous-liste de L à la position
+% N et que M est identique à L sans la sous-liste en question
+%
+
+
+
+%
+% inverse(L, M) si M contient les éléments de L en ordre inverse.
+%
+
+inverse(L, M) :- aux_inverse(L, [], M).
+
+aux_inverse([], Acc, Acc) :- !.
+
+aux_inverse([X|Xs], Acc, Inv) :-
+    aux_inverse(Xs, [X|Acc], Inv).
+        
+%
 % enleve_n(N, L1, L2) est vrai si L2 est la liste L1 sans ses N premiers éléments
 %
 
@@ -57,10 +105,6 @@ enleve_n(N, [_|Xs], L) :-
     enleve_n(N1, Xs, L),
     N is N1 + 1.
 
-%enleve_n(N, [_|Xs], L) :-
-%	N1 is N - 1,
-%    enleve_n(N1, Xs, L).
-   
 %
 % prend_n(N, L1, L2) est vrai si L2 contient les N premiers éléments de L1
 %
@@ -90,6 +134,143 @@ insere_enieme(N, L, E, M) :-
     enleve_n(N, L, Suf),
     concatenation(Pref, [E|Suf], M).
 
+%
+% trancher(L, P, D, M) si M est la sous-liste de L allant des indices
+% P à D.
+%
+
+trancher(L, P, D, M) :-
+    Tranche is D - P + 1,
+    enleve_n(P, L, Suf),
+    prend_n(Tranche, Suf, M).
+
+%
+% enleve_premiere_occurrence(L, E, M) est vrai si M est identique à L sauf que la 
+% premiere occurrence de E a été retirée.
+%
+
+enleve_premiere_occurrence([], _, []) :- !.
+
+enleve_premiere_occurrence([E|Xs], E, Xs) :- !.
+
+enleve_premiere_occurrence([X|Xs], E, [X|M]) :-
+    enleve_premiere_occurrence(Xs, E, M).
+
+%
+% enleve_toutes_occurrences(L, E, M) la liste M est identique à L mais ne contient aucun E
+%
+
+enleve_toutes_occurrences([], _, []) :- !.
+
+enleve_toutes_occurrences([E|Xs], E, M) :-
+    enleve_toutes_occurrences(Xs, E, M), !.
+
+enleve_toutes_occurrences([X|Xs], E, [X|M]) :-
+    \+(X=E), 
+    enleve_toutes_occurrences(Xs, E, M).
+
+%
+% elements_pairs(L, P) est vrai si P contient les éléments pairs de L
+%
+
+elements_pairs([], []).
+
+elements_pairs([X|Xs], [X|Ps]) :- 
+    (X mod 2 =:= 0), 
+    elements_pairs(Xs, Ps).
+
+elements_pairs([X|Xs], Ps) :- 
+    (X mod 2 =\= 0), 
+    elements_pairs(Xs, Ps).
+
+%
+% elements_pairs_n(L, P, N) est vrai si P contient les éléments pairs de L et N le cardinal de P
+%
+
+elements_pairs_n([], [], 0).
+
+elements_pairs_n([X|Xs], [X|Ps], N) :- 
+    (X mod 2 =:= 0), 
+    elements_pairs_n(Xs, Ps, N1), 
+    N is N1+1.
+
+elements_pairs_n([X|Xs], Ps, N) :- 
+    (X mod 2 =\= 0), 
+    elements_pairs_n(Xs, Ps, N).
+
+%
+% elements_parite(L, P, I, Np, Ni) est vrai si P contient les éléments pairs de L et Np le cardinal de P,
+% et si I contient les éléments impairs de L et Ni le cardinal de I.
+%
+
+elements_parite([], [], [], 0, 0).
+
+elements_parite([X|Xs], [X|Ps], I, Np, Ni) :- 
+    (X mod 2 =:= 0),
+    elements_parite(Xs, Ps, I, Np1, Ni),
+    Np is Np1 + 1.
+
+elements_parite([X|Xs], P, [X|Is], Np, Ni) :-
+    (X mod 2 =\= 0),
+    elements_parite(Xs, P, Is, Np, Ni1),
+    Ni is  Ni1 + 1.
+
+%
+% tri fusion(L, Lt) est vrai si Lt contient tous les éléments de L en ordre croissant.
+%
+
+fusion(L, [], L) :- !.
+
+fusion([], L, L) :- !.
+
+fusion([G|Gs], [D|Ds], [G|F]) :-
+    G < D,
+    fusion(Gs, [D|Ds], F).
+
+fusion([G|Gs], [D|Ds], [D|F]) :-
+    D =< G,
+    fusion([G|Gs], Ds, F).
+
+separe(L, Lg, Ld) :-
+    cardinal(L, N),
+    Mid is div(N, 2)-1,
+    trancher(L, 0, Mid, Lg),
+    Midp1 is Mid + 1,
+    Nm1 is N - 1,
+    trancher(L, Midp1, Nm1, Ld).
+
+tri_fusion([], []) :- !.
+
+tri_fusion([X], [X]) :- !.
+
+tri_fusion(L, Lt) :-
+    separe(L, Lg, Ld),
+    tri_fusion(Lg, Lgt),
+    tri_fusion(Ld, Ldt),
+    fusion(Lgt, Ldt, Lt).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%
+%
+%
 
 
 
--- testlistes.pl
+++ testlistes.pl
@@ -67,6 +67,15 @@ lt_six([1, 2, 3, 4, 5, 6]).
 ?- expect_true("liste 6 élément valide", element(1, [1, 2, 3, 4, 5, 6])).
 ?- expect_true("liste 6 dernier élément", element(6, [1, 2, 3, 4, 5, 6])).
 ?- expect_false("liste 6 élément invalide", element(26, [1, 2, 3, 4, 5, 6])).
+
+%
+% element/3
+%
+
+?- expect_true("Liste vide 0 occurrences", element(1, [], 0)).
+?- expect_true("Liste 6 éléments, un présent", element(3, [1, 2, 3, 4, 5], 1)).
+?- expect_true("Liste 6 éléments, répétition", element(3, [1, 3, 2, 3, 4, 3], 3)).
+
 %
 % concatenation
 %
@@ -78,7 +87,7 @@ lt_six([1, 2, 3, 4, 5, 6]).
 ?- expect_false("lt_six = lt_un+lt_36, faux", (lt_un(L1), lt_36(L36), lt_six(L6), concatenation(L1, L36, L6))).
 
 %
-% prefixe
+% prefixe/2
 %
 
 ?- expect_true("liste prefixe d'elle-même", (lt_six(L6), prefixe(L6, L6))).
@@ -86,6 +95,16 @@ lt_six([1, 2, 3, 4, 5, 6]).
 ?- expect_true("lt_deux prefixe de lt_trois", (lt_deux(L2), lt_trois(L3), prefixe(L2, L3))).
 ?- expect_true("lt_deux prefixe de lt_six", (lt_deux(L2), lt_six(L6), prefixe(L2, L6))).
 ?- expect_false("lt_26 prefixe lt_six faux", (lt_26(L26), lt_six(L6), prefixe(L26, L6))).
+
+%
+% prefixe/3
+%
+
+?- expect_true("liste prefixe d'elle-même", (lt_six(L6), prefixe(L6, L6, []))).
+?- expect_true("liste vide est un prefixe", (prefixe([], [1, 2], [1, 2]))).
+?- expect_true("lt_deux prefixe de lt_trois", (lt_deux(L2), lt_trois(L3), prefixe(L2, L3, [3]))).
+?- expect_true("lt_deux prefixe de lt_six", (lt_deux(L2), lt_six(L6), prefixe(L2, L6, [3, 4, 5, 6]))).
+?- expect_false("lt_26 prefixe lt_six faux", (lt_26(L26), lt_six(L6), prefixe(L26, L6, _))).
 
 %
 % suffixe
@@ -104,6 +123,25 @@ lt_six([1, 2, 3, 4, 5, 6]).
 ?- expect_true("sous-liste valide de L6", (lt_six(L6), sous_liste([3, 4, 5], L6))).
 ?- expect_false("sous-liste non-valide de L6", (lt_six(L6), sous_liste([1, 3, 4], L6))).
 ?- expect_false("sousl-liste trop longue de L6", (lt_six(L6), sous_liste([0, 1, 2, 3, 4, 5, 6], L6))).
+
+%
+% sous_liste
+%
+
+?- expect_true("liste vide est une sous-liste", (lt_six(L6), sous_liste([], L6, 0))).
+?- expect_true("liste est une sous-liste d'elle-même", (lt_six(L6), sous_liste(L6, L6, 0))).
+?- expect_true("sous-liste valide de L6", (lt_six(L6), sous_liste([3, 4, 5], L6, 2))).
+?- expect_false("sous-liste non-valide de L6", (lt_six(L6), sous_liste([1, 3, 4], L6, _))).
+?- expect_false("sousl-liste trop longue de L6", (lt_six(L6), sous_liste([0, 1, 2, 3, 4, 5, 6], L6, _))).
+
+%
+% inverse
+%
+
+?- expect_true("inverse vide", (inverse([], []))).
+?- expect_true("inverse singleton", (inverse([a], [a]))).
+?- expect_true("inverse éléments répétés", (inverse([a, a, a, a], [a, a, a, a]))).
+?- expect_true("inverse valide", (inverse([a, b, c, d], [d, c, b, a]))).
 
 %
 % enleve_n
@@ -146,13 +184,61 @@ lt_six([1, 2, 3, 4, 5, 6]).
 ?- expect_true("insere en fin", (lt_six(L6), insere_enieme(6, L6, 23, [1, 2, 3, 4, 5, 6, 23]))).
 ?- expect_false("insertion non valide", (lt_six(L6), insere_enieme(8, L6, 23, _))).
 
+%
+% trancher
+%
 
+?- expect_true("trancher valide", trancher([1, 2, 3, 4, 5], 1, 3, [2, 3, 4])).
+%
+% enleve_premiere_occurrence
+%
 
+?- expect_true("enleve premiere occurrence milieu", (enleve_premiere_occurrence([1, 2, 3], 2, [1, 3]))).
+?- expect_true("enleve premiere occurrence debut", (enleve_premiere_occurrence([1, 2, 3], 1, [2, 3]))).
+?- expect_true("enleve premiere occurrence fin", (enleve_premiere_occurrence([1, 2, 3], 3, [1, 2]))).
+?- expect_true("enleve premiere occurrence multiples", (enleve_premiere_occurrence([1, 1, 1], 1, [1, 1]))).
+?- expect_true("enleve premiere occurrence absent", (enleve_premiere_occurrence([1, 2, 3], 8, [1, 2, 3]))).
 
+%
+% enleve_toutes_occurrences
+%
 
+?- expect_true("enleve toutes occurrences", (enleve_toutes_occurrences([1, 1, 2, 1, 3, 4, 1, 6], 1, [2, 3, 4, 6]))).
+?- expect_true("enleve toutes occurences absent", (enleve_toutes_occurrences([1, 1, 2, 1, 3, 4, 1, 6], 57, [1, 1, 2, 1, 3, 4, 1, 6]))).
+?- expect_true("enleve_toutes_occurrences_vide", (enleve_toutes_occurrences([], 1, []))).
+?- expect_true("enleve_toutes_occurrences_total", (enleve_toutes_occurrences([1, 1, 1, 1, 1], 1, []))).
 
+%
+% elements_pairs
+%
 
+?- expect_true("elements pairs vide est vide", (elements_pairs([], []))).
+?- expect_true("elements pairs tous impairs", (elements_pairs([1, 3, 5, 7, 9], []))).
+?- expect_true("elements pairs tous pairs", (elements_pairs([0, 2, 4, 6, 8], [0, 2, 4, 6, 8]))).
+?- expect_true("elements pairs mixtes", (elements_pairs([0, 1, 2, 3, 4, 5], [0, 2, 4]))).
 
+%
+% elements_pairs_n
+%
+
+?- expect_true("elements pairs n vide est vide", (elements_pairs_n([], [], 0))).
+?- expect_true("elements pairs n tous impairs", (elements_pairs_n([1, 3, 5, 7, 9], [], 0))).
+?- expect_true("elements pairs n tous pairs", (elements_pairs_n([0, 2, 4, 6, 8], [0, 2, 4, 6, 8], 5))).
+?- expect_true("elements pairs n mixtes", (elements_pairs_n([0, 1, 2, 3, 4, 5], [0, 2, 4], 3))).
+
+%
+% elements_parite
+%
+
+?- expect_true("elements parite vide est vide", (elements_parite([], [], [], 0, 0))).
+?- expect_true("elements parite tous impairs", (elements_parite([1, 3, 5, 7, 9], [], [1, 3, 5, 7, 9], 0, 5))).
+?- expect_true("elements parite tous pairs", (elements_parite([0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [], 5, 0))).
+?- expect_true("elements parite mixtes", (elements_parite([0, 1, 2, 3, 4, 5], [0, 2, 4], [1, 3, 5], 3, 3))).
+?- expect_false("elements parite mixtes faux", (elements_parite([0, 1, 2, 3, 4, 5], [0, 4], [1, 3, 5], 1, 3))).
+
+%
+%
+%
 
 
 
